87d5cda458cb68c4ca01900df8d08c9fda2e67cd
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-11-21 22:36:11 +0000

    Adding cool new camera thing

--- ariadne/main.py
+++ ariadne/main.py
@@ -6,12 +6,21 @@ back_id = None
 
 MAX_BLOCKS = 20
 BLOCK_DIM = 1.0 / MAX_BLOCKS
+p_x = 0
+p_y = 18
+start_pos_x = BLOCK_DIM * 2 * p_x + BLOCK_DIM - 1.0
+start_pos_y = BLOCK_DIM * 2 * p_y + BLOCK_DIM - 1.0
+
 
 class KeyCodes:
     W = 119
     A = 97
     S = 115
     D = 100
+    UP = 1073741906
+    DOWN = 1073741905
+    LEFT = 1073741904
+    RIGHT = 1073741903
 
 
 def block_init(btype, x, y):
@@ -42,6 +51,16 @@ def player_init(x, y):
     return player_id
 
 
+def overlay_init(x, y):
+    overlay_id = cog.sprite_add('media/overlay.png')
+    overlay = cog.sprite_get(overlay_id)
+    overlay.dim.w = 8.0
+    overlay.dim.h = 8.0
+    overlay.pos.x = x
+    overlay.pos.y = y
+    return overlay_id
+
+
 def game_init():
     #back
     back_id = cog.sprite_add("media/back.png")
@@ -63,24 +82,31 @@ def game_init():
             y -= 1
             x = 0
     p = player_init(0, 18)
-    return (p, blocks)
+    o = overlay_init(0, 0)
+    return (p, blocks, o)
 
 
-def game_mainloop(player_id, block_ids):
+def game_mainloop(player_id, block_ids, overlay_id):
     player = cog.sprite_get(player_id)
+    overlay = cog.sprite_get(overlay_id)
     for b in block_ids:
         if(cog.sprite_collides_sprite(player_id, b)):
             print("Collision")
+            player.pos.x = start_pos_x
+            player.pos.y = start_pos_y
+            '''
             #TODO: Response properly
             player.pos.x = player.pos.x - (player.vel.x * 10)
             player.vel.y = player.pos.y - (player.vel.y * 10)
             player.vel.x = 0
             player.vel.y = 0
             break
+            '''
     if cog.input_key_pressed():
         key_code = cog.input_key_code_pressed()
         print("KEYCODE {}".format(key_code))
         vel_delta = 0.0001
+        cam_vel_delta = 0.0005
         if key_code == KeyCodes.A:
             player.vel.x = -vel_delta
         if key_code == KeyCodes.D:
@@ -89,6 +115,14 @@ def game_mainloop(player_id, block_ids):
             player.vel.y = vel_delta
         if key_code == KeyCodes.S:
             player.vel.y = -vel_delta
+        if key_code == KeyCodes.LEFT:
+            overlay.vel.x = -cam_vel_delta
+        if key_code == KeyCodes.RIGHT:
+            overlay.vel.x = cam_vel_delta
+        if key_code == KeyCodes.UP:
+            overlay.vel.y = cam_vel_delta
+        if key_code == KeyCodes.DOWN:
+            overlay.vel.y = -cam_vel_delta
     if cog.input_key_depressed():
         key_code = cog.input_key_code_depressed()
         print("KEYCODE DEPRESSED {}".format(key_code))
@@ -100,12 +134,20 @@ def game_mainloop(player_id, block_ids):
             player.vel.y = 0
         if key_code == KeyCodes.S:
             player.vel.y = 0
+        if key_code == KeyCodes.LEFT:
+            overlay.vel.x = 0
+        if key_code == KeyCodes.RIGHT:
+            overlay.vel.x = 0
+        if key_code == KeyCodes.UP:
+            overlay.vel.y = 0
+        if key_code == KeyCodes.DOWN:
+            overlay.vel.y = 0
 
 
 if __name__ == "__main__":
     cog.init()
-    player_id, block_ids = game_init()
+    player_id, block_ids, overlay_id = game_init()
     while not cog.hasquit():
         cog.loopstep()
-        game_mainloop(player_id, block_ids)
+        game_mainloop(player_id, block_ids, overlay_id)
     cog.quit()
